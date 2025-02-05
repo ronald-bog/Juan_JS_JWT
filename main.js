e05480f77140459f4b03b1ede60d47a75c5800b9
Ronald Espitia <ronald79@gmail.com>
2025-02-04 21:14:54 -0500

    Clase

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 // Crear un token JWT
 // sign(payload, key, opciones)
+// como llamar a la variable de entorno: process.env.CLAVE_SECRETA
+const key = process.env.CLAVE_SECRETA;
 
-const clave = 'abc';
+//const clave = 'abc';
 
 const payload = {
     userID: 101,
@@ -11,7 +14,21 @@ const payload = {
     role: 'admin'
 };
 
-const token = jwt.sign(payload, clave, { expiresIn: '1h' });
+const token = jwt.sign(payload, key, { expiresIn: '1h' });
 
 console.log(`Token generado: ${token}`);
+
+// Verificacion del token
+// sintaxis: jwt.verify(token, key)
+
+const token1 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOjEwMSwidXNlck5hbWUiOiJKdWFuIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzM4NzIwNDc4LCJleHAiOjE3Mzg3MjQwNzh9.9a_UEWXIBhrySdH2-34BQi9iqvMdEaPQOFhMv4-qpO4';
+
+try {
+    const tokenVerif = jwt.verify(token1, key);
+    //console.log(`Token Verificado: ${ tokenVerif }`);
+    console.log('Token Verificado:', tokenVerif);
+
+} catch (error) {
+    console.error('TOKEN INVALIDO: ', error.message);
+}
 
